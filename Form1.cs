9c20231da6262a3ec000469a4543787feaa5688a
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2024-12-04 23:39:14 +0800

    Fix collision, optimize code

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,7 @@ namespace WinFormsPhysics {
 
     private void PhysicsCheckbox_CheckedChanged(object sender, EventArgs e) {
       if (physicsCheckbox.Checked) {
+        Engine.ClearVelocity();
         UpdateTimer.Start();
       } else {
         UpdateTimer.Stop();
@@ -130,43 +131,36 @@ namespace WinFormsPhysics {
       if (Objects.Count == 0) return;
 
       var netForces = new Vector[Objects.Count];
-      for (int i = 0; i < netForces.Length; i++) {
+      for (int i = 0; i < Objects.Count; i++) {
         netForces[i] = new(0, 0);
       }
 
-      // Compute gravitational force from space
-      if (GravityAcc.X != 0 || GravityAcc.Y != 0) {
-        for (int i = 0; i < Objects.Count; i++) {
+      // Compute gravitational forces
+      for (int i = 0; i < Objects.Count; i++) {
+        // From space
+        if (GravityAcc.X != 0 || GravityAcc.Y != 0) {
           double gfX = Objects[i].Mass * GravityAcc.X;
           double gfY = Objects[i].Mass * GravityAcc.Y;
           netForces[i] += new Vector(gfX, gfY);
         }
-      }
-       
-      // Compute gravitational force from other objects
-      if (GravityConst != 0) {
-        for (int i = 0; i < Objects.Count - 1; i++) {
+
+        // From other objects
+        if (GravityConst != 0) {
           for (int j = i + 1; j < Objects.Count; j++) {
             Object a = Objects[i], b = Objects[j];
             Vector posA = a.Position, posB = b.Position;
 
             double distance = Distance(posA, posB);
-
-            // Avoid division by zero
             if (distance == 0) continue;
 
             double f = (GravityConst * a.Mass * b.Mass) / (distance * distance);
-
             double deltaX = posB.X - posA.X;
             double deltaY = posB.Y - posA.Y;
 
-            Vector force = new(
-              f * deltaX / distance,
-              f * deltaY / distance
-            );
+            Vector force = new(f * deltaX / distance, f * deltaY / distance);
 
             netForces[i] += force;
-            netForces[j] -= force; // Opposite direction
+            netForces[j] -= force;
           }
         }
       }
@@ -178,9 +172,10 @@ namespace WinFormsPhysics {
         obj.Velocity += acceleration * TimeLapse;
       }
 
-      // Compute collision with borders
-      if (ScreenBounds) {
-        for (int i = 0; i < Objects.Count; i++) {
+      // Compute collisions
+      for (int i = 0; i < Objects.Count; i++) {
+        // With borders
+        if (ScreenBounds) {
           Object obj = Objects[i];
           Form form = obj.BodyForm;
           Vector v = obj.Velocity;
@@ -192,23 +187,37 @@ namespace WinFormsPhysics {
             obj.Velocity = new Vector(v.X, -v.Y) * 0.8;
           }
         }
-      }
 
-      // Compute collision with other objects
-      for (int i = 0; i < Objects.Count - 1; i++) {
+        // With other objects
         for (int j = i + 1; j < Objects.Count; j++) {
           Object a = Objects[i], b = Objects[j];
 
           ObjectForm formA = a.BodyForm, formB = b.BodyForm;
 
-          if (!formA.Bounds.IntersectsWith(formB.Bounds)) continue;
+          Rectangle overlap = Rectangle.Intersect(formA.Bounds, formB.Bounds);
 
-          if (b.BodyForm.FixedPos) {
-            a.Velocity = ~a.Velocity * 0.9;
+          if (overlap == Rectangle.Empty) continue;
+          
+          bool horizontalCollision = overlap.Height > overlap.Width;
+
+          if (formB.FixedPos) {
+            if (horizontalCollision) {
+              a.Velocity.X *= -1;
+              a.Velocity *= 0.9;
+            } else {
+              a.Velocity.Y *= -1;
+              a.Velocity *= 0.9;
+            }
             break;
           }
-          if (a.BodyForm.FixedPos) {
-            b.Velocity = ~b.Velocity * 0.9;
+          if (formA.FixedPos) {
+            if (horizontalCollision) {
+              b.Velocity.X *= -1;
+              b.Velocity *= 0.9;
+            } else {
+              b.Velocity.Y *= -1;
+              b.Velocity *= 0.9;
+            }
             break;
           }
 
@@ -221,7 +230,7 @@ namespace WinFormsPhysics {
         }
       }
 
-      // Apply force to objects
+      // Apply velocity to objects
       for (int i = 0; i < Objects.Count; i++) {
         Object obj = Objects[i];
         ObjectForm form = obj.BodyForm;
@@ -252,108 +261,6 @@ namespace WinFormsPhysics {
         }
 
         form.Location = newPos.ToPoint();
-      }
-    }
-  }
-
-
-
-  public class Bounce : Control {
-    private class Vector2D {
-      public double X { get; set; }
-      public double Y { get; set; }
-
-      public static Vector2D operator +(Vector2D a, Vector2D b) => new Vector2D(a.X + b.X, a.Y + b.Y);
-      public static Vector2D operator -(Vector2D a, Vector2D b) => new Vector2D(a.X - b.X, a.Y - b.Y);
-      public static Vector2D operator *(Vector2D a, Vector2D b) => new Vector2D(a.X * b.X, a.Y * b.Y);
-      public static Vector2D operator /(Vector2D a, Vector2D b) => new Vector2D(a.X / b.X, a.Y / b.Y);
-      public static Vector2D operator ~(Vector2D a) => new Vector2D(-a.X, -a.Y);
-
-      public void clip(Vector2D bounds) {
-        clip(bounds, ~bounds);
-      }
-      public void clip(Vector2D upper, Vector2D lower) {
-        if (X > upper.X) X = upper.X;
-        if (X < lower.X) X = lower.X;
-        if (Y > upper.Y) Y = upper.Y;
-        if (Y < lower.Y) Y = lower.Y;
-      }
-
-      public Point ToPoint() => new((int)X, (int)Y);
-
-      public Vector2D(double x, double y) {
-        X = x;
-        Y = y;
-      }
-      public Vector2D(Point p) {
-        X = p.X;
-        Y = p.Y;
-      }
-    }
-
-    Vector2D Gravity { get; set; } = new Vector2D(0, 1f);
-    Vector2D Elasticity { get; set; } = new Vector2D(0.5f, 0.6f);
-    Vector2D MaxVelocity { get; set; } = new Vector2D(100f, 100f);
-    Vector2D Friction { get; set; } = new Vector2D(0.7f, 1f);
-
-    Vector2D mouseDragStart = new Vector2D(0, 0);
-    System.Windows.Forms.Timer updateTimer = new System.Windows.Forms.Timer();
-    Form parent = null;
-    Vector2D velocity = new Vector2D(0, 0);
-    bool enabled = true;
-
-    public Bounce() {
-      ParentChanged += parentChanged;
-    }
-
-    private void parentChanged(object sender, EventArgs e) {
-      if (parent != null)
-        throw new Exception("Parent form cannot be changed!");
-
-      parent = Parent as Form ?? throw new Exception("Control must be added to a form!");
-
-      parent.ResizeBegin += dragStart;
-      parent.ResizeEnd += dragEnd;
-
-      updateTimer.Tick += update;
-      updateTimer.Interval = 10;
-      updateTimer.Start();
-    }
-
-    private void dragStart(object sender, EventArgs e) {
-      mouseDragStart = new Vector2D(Cursor.Position);
-      // Prevent location updates when user drags form
-      enabled = false;
-    }
-    private void dragEnd(object sender, EventArgs e) {
-      velocity = new Vector2D(Cursor.Position) - mouseDragStart;
-      enabled = true;
-    }
-
-    private void update(object sender, EventArgs e) {
-      if (Enabled) {
-        if (enabled) {
-          velocity += Gravity;
-          velocity.clip(MaxVelocity);
-
-          var screenBounds = Screen.GetWorkingArea(parent.Location);
-
-          Vector2D nextLoc = new Vector2D(parent.Location) + velocity;
-
-          if (nextLoc.X <= screenBounds.Left || nextLoc.X >= screenBounds.Right - parent.Size.Width)
-            velocity.X *= -Elasticity.X;
-
-          if (nextLoc.Y >= screenBounds.Bottom - parent.Size.Height || nextLoc.Y <= screenBounds.Top) {
-            velocity.Y *= -Elasticity.Y;
-            velocity *= Friction;
-          }
-
-          nextLoc.clip(new Vector2D(screenBounds.Right - parent.Size.Width, screenBounds.Bottom - parent.Size.Height), new Vector2D(screenBounds.Left, screenBounds.Top));
-
-          parent.Location = nextLoc.ToPoint();
-        } else {
-          mouseDragStart = new Vector2D(Cursor.Position);
-        }
       }
     }
   }
